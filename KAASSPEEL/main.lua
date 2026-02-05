9c95905f9738786413f9b6483803ca637d6dd291
Bohdan <178602384+s00ndie@users.noreply.github.com>
2026-02-05 13:16:52 +0100

    music

--- KAASSPEEL/main.lua
+++ KAASSPEEL/main.lua
@@ -1,4 +1,10 @@
+display.setStatusBar(display.HiddenStatusBar)
+
 local composer = require("composer")
+
+local bgMusic = audio.loadStream('bgmusic.mp3')
+audio.setVolume(0.6)
+audio.play(bgMusic,{ loops = -1})
 
 composer.gotoScene("menu")
 
